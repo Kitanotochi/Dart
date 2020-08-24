469efd3a031ff9687e3e6840f2dd232684b218f9
Kitanotochi <58184800+Kitanotochi@users.noreply.github.com>
2020-08-24 20:17:52 +0300

    Create 03_Data_type.dart

---
+++ learning_dart/03_Data_type.dart
@@ -0,0 +1,21 @@
+// Типы данных в Dart
+
+void main() {
+  // Number (содержит в себе два типа: int и double)
+  double my_weight = 170.5;
+  print(my_weight.runtimeType);
+
+  // Boolean (этот тип предствален двумя значениями: true и false)
+  bool isApple = true;
+  print(isApple.runtimeType);
+
+  // String (строки имеют последовательность символов в кодировке UTF-16)
+  // Это единственный тип данных, который пишется с заглавной буквы
+  // Для определения строки, можно использовать как одинарные, так и двойные ковычки
+  String my_name = 'Sergei'
+  print(my_name.runtimeType);
+
+  // Runes
+
+  // Symbol
+}
