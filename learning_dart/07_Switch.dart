dffeb5b1a240ab3da1c19b8ace014c8b81c33366
Kitanotochi <58184800+Kitanotochi@users.noreply.github.com>
2020-09-20 12:55:36 +0300

    Update 07_Switch.dart

--- learning_dart/07_Switch.dart
+++ learning_dart/07_Switch.dart
@@ -1,3 +1,19 @@
 // Конструкция "switch" в языке Dart
 
-void main() {}
+void main() {
+
+  // Switch / Case
+  String grade = "B";
+
+  switch(grade) {
+    case "A":
+      print("Exelent grade of A");
+      break;
+    case "B":
+      print("Very Good!");
+      break;
+    default:
+      print("Failed");
+  }
+
+}
