a8bdba759d5e014fca62c5917cbee11545eb3cfd
Kitanotochi <58184800+Kitanotochi@users.noreply.github.com>
2020-09-25 16:50:59 +0300

    Create 09_While.dart

---
+++ learning_dart/09_While.dart
@@ -0,0 +1,10 @@
+// Цикл "while" в языке Dart
+
+void main() {
+  int counter = 0;
+  // Цикл выполнился десять раз, но условие проверяется перед входом в цикл
+  while(counter < 10) { 
+    print("Hello Dart $counter");
+    counter++;
+  }
+}
