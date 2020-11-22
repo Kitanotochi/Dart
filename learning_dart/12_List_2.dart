ac33cd5a366c2ffd85de5492dc5d2d3262928fd0
Kitanotochi <58184800+Kitanotochi@users.noreply.github.com>
2020-11-22 16:43:05 +0300

    Update 12_List_2.dart

--- learning_dart/12_List_2.dart
+++ learning_dart/12_List_2.dart
@@ -1,5 +1,13 @@
 // List (список переменной длины) в языке Dart
 
 void main() {
-  
+  List numberList = List(); // Растущий список
+    numberList.add(22); // Передаем значение для нашего будущего элемента
+    numberList.add(33);
+    numberList.add("hello");
+    numberList.add(true);
+    numberList.add(22.0);
+
+    print(numberList[1]; // Обращаться и обновлять значения в переменном списке, можно так же как и в фиксированном
+     
 }
