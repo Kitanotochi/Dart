5c28b1482a5ef61985c6f9625eec8ba61c22e415
Kitanotochi <58184800+Kitanotochi@users.noreply.github.com>
2020-12-13 20:52:21 +0300

    Update 13_Set.dart

--- learning_dart/13_Set.dart
+++ learning_dart/13_Set.dart
@@ -1,6 +1,15 @@
 // Set в языке Dart
 
-void main(List<String> args) {
-  
-  
+void main() {
+  var set = {1,2,3,4}; // Создаем "set"
+  print(set);
+
+  Set <int> set1 = {1,2,3,4}; // Объявляем конструкцию явно, используюя обобщенный тип
+  print(set1);
+
+  var set2 = <int>{1,2,3,4} // Еще один вариант
+  print(set2);
+
+  Set set3 = Set.from([1, "hello", true]); // Создаем "set" из списка
+  print(set3);
 }
